b10c20718d5684cadbee4551759e6a751db3e714
Nilesh Yadav <nileshyaduvanshi@outlook.com>
2023-05-06 09:28:07 +0530

    initial commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'pass', usernameVariable: 'user')]) {
+        sh 'ls'
+        sh 'docker build -t javaapp . '
+        sh "echo $pass | docker login -u $user  --password-stdin"
+        sh 'docker tag javaapp nileshyav/sample-java-spring-app:${BUILD_NUMBER}'
+        sh 'docker push nileshyav/sample-java-spring-app:${BUILD_NUMBER} '
+    }
+}
